7af50992cb91d2cabaa39692053e5266f4a4f65f
JohnnyC <j0hnnywchiang@gmail.com>
2018-09-18 14:23:39 -0700

    reorganize file structure

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-require("./app/routes/apiRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
